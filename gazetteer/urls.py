41c1596d674ba21781b82b6b4f6651ae74c39b64
Matthew Somerville <matthew-github@dracos.co.uk>
2013-03-16 13:34:05 +0000

    Update Django to 1.5.

--- gazetteer/urls.py
+++ gazetteer/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns
 
 urlpatterns = patterns('gazetteer.views',
     (r'^$', 'home'),
--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -19,10 +20,5 @@ urlpatterns = patterns('',
     (r'^', include('gazetteer.urls')),
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT
-        })
-    )
+urlpatterns += staticfiles_urlpatterns()
 
